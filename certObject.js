04864b149cc5103fa5dd50486d22546a11eca01f
TheZoom110 <soukaryacoder@gmail.com>
2024-05-07 06:11:29 +0530

    Delete gitignore, upload certObject

---
+++ certObject.js
@@ -0,0 +1,4 @@
+const certLinks = {
+    "24TONEWA003": ["1nIfP7SyzKDpG7LQ97QCLZhPLKGUaKIU2", "Samrat Mondal", "14th March 2024"],
+    "24TONCDA003": ["1xw5u3M4zQlSdzylGpATzR-rmf5glAZ-x", "Samrat Mondal", "14th March 2024"]
+}
